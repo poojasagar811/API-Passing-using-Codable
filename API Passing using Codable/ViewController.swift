2070e06dcb5031d3d78f1244b73460467fc81a52
pooja sagar <poojasagar811@gmail.com>
2021-07-20 13:54:46 +0530

    Initial Commit

---
+++ API Passing using Codable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API Passing using Codable
+//
+//  Created by Pooja's MacBook Pro on 20/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
